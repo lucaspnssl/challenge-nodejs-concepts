557114ab7208c240234f00fcc119634a6630c982
Lucas Panassolo <lucaspanassolo12@hotmail.com>
2021-01-10 19:18:50 -0300

    Application created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
+function getReposIndexByID(id) {
+  return repositories.findIndex(repos => repos.id === id);
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+    const repos = { id: uuid(), title, url, techs, likes: 0 };
+
+    repositories.push(repos);
+    return response.json(repos);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = getReposIndexByID(id);
+
+  if (index < 0)
+    return response.status(400).json({ error: 'Repository not found.'});
+
+  const repos = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[index].likes
+  };
+
+  return response.json(repos); 
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = getReposIndexByID(id);
+
+  if (index < 0)
+    return response.status(400).json({ error: 'Repository not found.' });
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = getReposIndexByID(id);
+
+  if (index < 0)
+    return response.status(400).json({ error: 'Repository not found.' });
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
